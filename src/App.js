a574e6fe3076b147d7a975b3091bb898ba4e2ed5
Miles Cruz <cruz.r.miles@gmail.com>
2019-05-10 15:30:37 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My name is MILES
         </a>
       </header>
     </div>

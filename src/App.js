2279a6d8f16f2a8ead3f39e35fb0576aab5273b0
Miles Cruz <cruz.r.miles@gmail.com>
2019-05-10 15:26:52 -0500

    changed link for testing

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://Meiles.github.io/"
           target="_blank"
           rel="noopener noreferrer"
         >
